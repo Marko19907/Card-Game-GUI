09f554cfbffc7f8116506728de1109ff3dd546fc
Marko19907 <marko.stancic23@gmail.com>
2021-03-24 14:52:44 +0100

    Task 3, Added dealHand(int n) and created the PlayingHand class

--- src/main/java/no/ntnu/idata2001/oblig3/cardgame/DeckOfCards.java
+++ src/main/java/no/ntnu/idata2001/oblig3/cardgame/DeckOfCards.java
@@ -1,15 +1,25 @@
 package no.ntnu.idata2001.oblig3.cardgame;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
 
+/**
+ * Class DeckOfCards represents a full deck of 52 cards
+ * The class is responsible for managing the deck and returning sets of random cards
+ */
 public class DeckOfCards
 {
-    private final char[] suit = {'S', 'H', 'D', 'C'};
+    private final char[] suits = {'S', 'H', 'D', 'C'};
     private final HashMap<String, PlayingCard> deck;
+    private final Random randomGenerator;
 
     public DeckOfCards()
     {
         this.deck = new HashMap<>();
+        this.randomGenerator = new Random();
+
         this.createDeck();
     }
 
@@ -18,10 +28,9 @@ public class DeckOfCards
      */
     private void createDeck()
     {
-        for (char suit : this.suit) {
+        for (char suit : this.suits) {
             for (int rank = 1; rank <= 13; rank++) {
-                PlayingCard card = new PlayingCard(suit, rank);
-                this.addCard(card);
+                this.addCard(new PlayingCard(suit, rank));
             }
         }
     }
@@ -38,5 +47,64 @@ public class DeckOfCards
                 this.deck.put(card.getAsString(), card);
             }
         }
+    }
+
+    /**
+     * Returns a List of randomly selected cards with a specified size
+     * @param numberOfCards The number of cards to return,
+     *                      number must be between and including 1 and 52
+     * @return Returns a List of randomly selected cards,
+     * the List will be empty is the numberOfCards parameter is invalid or
+     * the numberOfCards is higher than the number of cards available in the deck
+     */
+    public List<PlayingCard> dealHand(int numberOfCards)
+    {
+        List<PlayingCard> toReturn = new ArrayList<>();
+        if ((numberOfCards >= 1) && (numberOfCards <= 52) && (numberOfCards <= this.getDeckSize())) {
+            int index = 0;
+            while (index < numberOfCards) {
+                toReturn.add(this.getRandomCard());
+                index++;
+            }
+        }
+        return toReturn;
+    }
+
+    /**
+     * Returns a randomly selected card from the deck
+     * @return One random card from the deck, null if the deck is empty
+     */
+    private PlayingCard getRandomCard()
+    {
+        PlayingCard card = null;
+        if (!this.deck.isEmpty()) {
+            ArrayList<PlayingCard> cards = new ArrayList<>(this.deck.values());
+            int randomIndex = this.randomGenerator.nextInt(cards.size());
+            card = cards.get(randomIndex);
+
+            this.removeCard(card);
+        }
+        return card;
+    }
+
+    /**
+     * Removes a given card from the deck
+     * @param card The card to remove,
+     *             can not be null
+     */
+    private void removeCard(PlayingCard card)
+    {
+        if (card != null) {
+            this.deck.remove(card.getAsString());
+        }
+    }
+
+    /**
+     * Returns the size of the deck
+     * @return the size of the deck as an int
+     */
+    private int getDeckSize()
+    {
+        return this.deck.size();
     }
 }
---
+++ src/main/java/no/ntnu/idata2001/oblig3/cardgame/PlayingHand.java
@@ -0,0 +1,6 @@
+package no.ntnu.idata2001.oblig3.cardgame;
+
+public class PlayingHand
+{
+
+}
